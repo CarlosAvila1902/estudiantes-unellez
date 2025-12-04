f858b22eb3988a917d9cc11a351ecce6c03d286e
Carlos Avila <dannny.u1@gmail.com>
2025-12-04 12:52:42 -0400

    fix: configuración de colecciones para Nuxt Content v3 y corrección de rutas

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
+<script setup lang="ts">
+// Pedimos los datos a la colección 'estudiantes'
+const { data: students } = await useAsyncData('lista-estudiantes', () => {
+  return queryCollection('estudiantes').all()
+})
+</script>
+
 <template>
   <main>
     <ContentDoc />
-    <ContentList path="/estudiantes" v-slot="{ list }">
-      <v-list lines="two">
-        <v-list-item v-for="student in list" :key="student._path" >
-          <!-- <pre>{{ student }}</pre> -->
-          <v-list-item-title>
-            <NuxtLink :to="student._path">{{ student.name }}</NuxtLink>
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </ContentList>
+
+    <v-list lines="two" v-if="students">
+      <v-list-item v-for="student in students" :key="student.stem">
+        <v-list-item-title>
+          <NuxtLink :to="'/' + student.stem">{{ student.name }}</NuxtLink>
+        </v-list-item-title>
+        
+        <v-list-item-subtitle v-if="student.aboutMe">
+          {{ student.aboutMe }}
+        </v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+
+    <div v-else>
+      No se encontraron estudiantes.
+    </div>
   </main>
 </template>
